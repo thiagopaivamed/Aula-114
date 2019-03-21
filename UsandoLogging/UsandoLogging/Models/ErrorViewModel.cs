4efabf2d67668e85b8acb2a1d4bf523db272cca3
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-21 07:18:04 -0300

    Usando logging

---
+++ UsandoLogging/UsandoLogging/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UsandoLogging.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
